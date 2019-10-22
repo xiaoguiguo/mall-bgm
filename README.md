d78ca84728c863558f5c39b82cc271e685b25f67
doudou <varuscn@gmail.com>
2019-10-22 11:37:51 +0800

    登录页，实现登录跳转

--- README.md
+++ README.md
@@ -17,3 +17,11 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# package.json
+babel-polyfill // 使低版本的浏览器支持ES6的语法
+mavon-editor //支持markdown编辑器
+vue-cropperjs //对图片的裁剪
+vue-quill-editor //富文本编辑器
+vue-schart //图表组件
+vuedraggable //拖动组件
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from "../components/page/Login"
+import Header from "../components/common/Header"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'header',
+    component: Header
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
